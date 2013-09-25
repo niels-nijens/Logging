22d94e8e45c253957429bb9a4bd838abf6674081
Niels Nijens <nijens.niels@gmail.com>
2013-09-25 22:56:46 +0200

    Added MemoryLogAdapter

---
+++ src/Adapters/MemoryLogAdapter.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AtomicPHP\Logging\Adapters;
+
+use \DateTime;
+
+/**
+ * MemoryLogger
+ *
+ * @author Niels Nijens <nijens.niels@gmail.com>
+ **/
+class MemoryLogAdapter extends AbstractLogAdapter
+{
+    /**
+     * The array with logged records
+     *
+     * @access protected
+     * @var    array
+     **/
+    protected $records = array();
+
+    /**
+     * write
+     *
+     * Writes the message to the log
+     *
+     * @access protected
+     * @param  string   $level
+     * @param  string   $message
+     * @param  array    $context
+     * @return boolean
+     **/
+    protected function write($level, $message, array $context = array() )
+    {
+        $record = array(
+            "timestamp" => new DateTime(),
+            "level" => $level,
+            "message" => $message,
+            "context" => $context,
+        );
+
+        array_push($this->records, $record);
+
+        return true;
+    }
+
+    /**
+     * getRecords
+     *
+     * Returns the array with logged records
+     *
+     * @access public
+     * @return array
+     **/
+    public function getRecords()
+    {
+        return $this->records;
+    }
+}
