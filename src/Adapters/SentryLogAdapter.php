fd1fe895ffa8dc5272b8f1d52cd6ac5ef5e05a1d
Niels Nijens <nijens.niels@gmail.com>
2013-09-29 18:34:02 +0200

    Added SentryLogAdapter

---
+++ src/Adapters/SentryLogAdapter.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace AtomicPHP\Logging\Adapters;
+
+use \Exception;
+use \Raven_Client;
+use \Psr\Log\LogLevel;
+
+/**
+ * SentryLogAdapter
+ *
+ * @author  Niels Nijens <nijens.niels@gmail.com>
+ * @package AtomicPHP\Logging\Adapters
+ * */
+class SentryLogAdapter extends AbstractLogAdapter
+{
+    /**
+     * The Raven_Client instance used to send the log messages to Sentry
+     *
+     * @access protected
+     * @var    Raven_Client
+     **/
+    protected $client;
+
+    /**
+     * __construct
+     *
+     * Creates a new log adapter instance
+     *
+     * @access public
+     * @param  array $channels
+     * @param  array $configuration
+     * @return LogAdapterInterface
+     **/
+    public function __construct(array $channels = array(), array $configuration = array() )
+    {
+        parent::__construct($channels, $configuration);
+
+        $this->client = new Raven_Client($this->getConfigurationValue("dsn") );
+    }
+
+    /**
+     * write
+     *
+     * Writes the message to the log
+     *
+     * @access protected
+     * @param  string   $level
+     * @param  string   $message
+     * @param  array    $context
+     * @return boolean
+     **/
+    protected function write($level, $message, array $context = array() )
+    {
+        if (array_key_exists("exception", $context) && $context["exception"] instanceof Exception) {
+            $data = array(
+                "level" => $this->translateLogLevelToSentryLevel($level),
+                "extra" => $context,
+            );
+
+            $this->client->captureException($context["exception"], $data);
+        }
+        else {
+            $this->client->captureMessage($message, $context, $this->translateLogLevelToSentryLevel($level) );
+        }
+    }
+
+    /**
+     * translateLogLevelToSentryLevel
+     *
+     * Returns a Raven_Client constant value for $level
+     *
+     * @access protected
+     * @param  string   $level
+     * @return string
+     **/
+    protected function translateLogLevelToSentryLevel($level)
+    {
+        switch ($level) {
+            case LogLevel::EMERGENCY:
+            case LogLevel::ALERT:
+            case LogLevel::CRITICAL:
+                $level = Raven_Client::FATAL;
+                break;
+            case LogLevel::ERROR:
+                $level = Raven_Client::ERROR;
+                break;
+            case LogLevel::WARNING:
+                $level = Raven_Client::WARNING;
+                break;
+            case LogLevel::NOTICE:
+            case LogLevel::INFO:
+                $level = Raven_Client::INFO;
+                break;
+            case LogLevel::DEBUG:
+                $level = Raven_Client::DEBUG;
+        }
+
+        return $level;
+    }
+}
