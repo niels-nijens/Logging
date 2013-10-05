bcd9a4409f70ceb7359d6a6b72208144e7beff1c
Niels Nijens <nijens.niels@gmail.com>
2013-10-05 22:37:46 +0200

    Removed debug code from PHPUnit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,8 +10,6 @@ spl_autoload_register(function($className) {
     $vendorNamespace = "AtomicPHP\\Logging\\";
     if (strpos($className, $vendorNamespace) === 0) {
         $classNameFile = str_replace("\\", DIRECTORY_SEPARATOR, substr($className, strlen($vendorNamespace) ) ) . ".php";
-        var_dump(__DIR__ . "/../src/" . $classNameFile, is_file(__DIR__ . "/../src/" . $classNameFile) );
-        var_dump(__DIR__ . "/../" . lcfirst($classNameFile), is_file(__DIR__ . "/../" . lcfirst($classNameFile) ) );
 
         if (is_file(__DIR__ . "/../src/" . $classNameFile) ) {
             include __DIR__ . "/../src/" . $classNameFile;
