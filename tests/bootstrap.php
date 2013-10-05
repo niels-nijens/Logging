7983a5fee75034c4e99dabe986ecc6a4fd8a83e1
Niels Nijens <nijens.niels@gmail.com>
2013-10-05 22:30:52 +0200

    Changes to PHPUnit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,12 +10,14 @@ spl_autoload_register(function($className) {
     $vendorNamespace = "AtomicPHP\\Logging\\";
     if (strpos($className, $vendorNamespace) === 0) {
         $classNameFile = substr($className, strlen($vendorNamespace) ) . ".php";
+        var_dump(__DIR__ . "/../src/" . $classNameFile, is_file(__DIR__ . "/../src/" . $classNameFile) );
+        var_dump(__DIR__ . "/../" . lcfirst($classNameFile), is_file(__DIR__ . "/../" . lcfirst($classNameFile) ) );
 
         if (is_file(__DIR__ . "/../src/" . $classNameFile) ) {
             include __DIR__ . "/../src/" . $classNameFile;
         }
-        elseif (is_file(__DIR__ . "/" . lcfirst($classNameFile) ) ) {
-            include __DIR__ . "/" . lcfirst($classNameFile);
+        elseif (is_file(__DIR__ . "/../" . lcfirst($classNameFile) ) ) {
+            include __DIR__ . "/../" . lcfirst($classNameFile);
         }
     }
 }, true);
