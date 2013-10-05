946ab51d8c307e9a09c8a8ccebbf8c9342ad06c9
Niels Nijens <nijens.niels@gmail.com>
2013-10-05 22:34:56 +0200

    Improvements to PHPUnit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,7 @@ require_once __DIR__ . "/../vendor/autoload.php";
 spl_autoload_register(function($className) {
     $vendorNamespace = "AtomicPHP\\Logging\\";
     if (strpos($className, $vendorNamespace) === 0) {
-        $classNameFile = substr($className, strlen($vendorNamespace) ) . ".php";
+        $classNameFile = str_replace("\\", DIRECTORY_SEPARATOR, substr($className, strlen($vendorNamespace) ) ) . ".php";
         var_dump(__DIR__ . "/../src/" . $classNameFile, is_file(__DIR__ . "/../src/" . $classNameFile) );
         var_dump(__DIR__ . "/../" . lcfirst($classNameFile), is_file(__DIR__ . "/../" . lcfirst($classNameFile) ) );
 
